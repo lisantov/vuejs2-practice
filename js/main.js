856359aba33f1c269cdd959d03b8c5bc6dac7642
lisantov <lisantov@yandex.ru>
2026-02-03 10:28:02 +0700

    Add: PW7, computed

--- js/main.js
+++ js/main.js
@@ -2,30 +2,50 @@ let app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-blue-onWhite.jpg',
+        brand: 'Vue Mastery',
         altText: 'A pair of socks',
-        inStock: true,
         details: ['80% Cotton', '20% Polyester', 'Gender-neutral'],
+        selectedVariant: 0,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 0,
+                onSale: false,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 10,
+                onSale: true,
             },
         ],
         cart: 0
+    },
+    computed: {
+        title() {
+            return `${this.product} ${this.brand}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            return this.variants[this.selectedVariant].onSale && this.inStock
+                ? `Распродажа ${this.title}! Осталось всего ${this.variants[this.selectedVariant].variantQuantity}`
+                : '';
+        }
     },
     methods: {
         addToCart() {
           this.cart++;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
         }
     }
 })
